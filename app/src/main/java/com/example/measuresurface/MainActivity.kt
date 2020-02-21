d5b64d28c6a53ee44a5d1edca97890ca1c082ad4
LouisBoucquet <louisboucquet@gmail.com>
2020-02-21 02:10:40 +0100

    yeet

--- app/src/main/java/com/example/measuresurface/MainActivity.kt
+++ app/src/main/java/com/example/measuresurface/MainActivity.kt
@@ -5,6 +5,8 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 
 import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,8 +26,43 @@ class MainActivity : AppCompatActivity() {
 
         // ARCore requires camera permission to operate.
         if (!CameraPermissionHelper.hasCameraPermission(this)) {
-            CameraPermissionHelper.requestCameraPermission(this);
-            return;
+            CameraPermissionHelper.requestCameraPermission(this)
+            return
+        }
+
+//        // Make sure Google Play Services for AR is installed and up to date.
+//        if (mSession == null) {
+//            switch (ArCoreApk.getInstance().requestInstall(this, mUserRequestedInstall)) {
+//                case INSTALLED:
+//                // Success, create the AR session.
+//                mSession = new Session(this);
+//                break;
+//                case INSTALL_REQUESTED:
+//                // Ensures next invocation of requestInstall() will either return
+//                // INSTALLED or throw an exception.
+//                mUserRequestedInstall = false;
+//                return;
+//            }
+//        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        results: IntArray
+    ) {
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            Toast.makeText(
+                this,
+                "Camera permission is needed to run this application",
+                Toast.LENGTH_LONG
+            )
+                .show()
+            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
+                // Permission denied with checking "Do not ask again".
+                CameraPermissionHelper.launchPermissionSettings(this)
+            }
+            finish()
         }
     }
 }
